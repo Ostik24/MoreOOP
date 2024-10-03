ecdbce83085ad6c3d95fafed3813b1c1bfd3219c
Ostap Pavlyshyn <ostappavlishin@gmail.com>
2024-10-03 12:16:13 +0300

    avoided using the dependency

--- src/main/java/lotr/Character.java
+++ src/main/java/lotr/Character.java
@@ -1,13 +1,30 @@
 package lotr;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter@Setter@AllArgsConstructor
 public abstract class Character {
     private int hp;
     private int power;
+
+    public Character(int hp, int power) {
+        this.hp = hp;
+        this.power = power;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    // Setters
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
 
     public abstract void kick(Character c);
 
