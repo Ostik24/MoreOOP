29efc1f0aa62b173be1d613bc5b898c660135acd
Ostap Pavlyshyn <ostappavlishin@gmail.com>
2024-10-03 12:18:02 +0300

    some fixes in Noble

--- src/main/java/lotr/Noble.java
+++ src/main/java/lotr/Noble.java
@@ -4,13 +4,11 @@ import java.util.Random;
 
 public abstract class Noble extends Character {
     public Noble(int min, int max) {
-        super(new Random().nextInt(min, max), new Random().nextInt(min, max));
+        super(min + new Random().nextInt(max - min + 1), min + new Random().nextInt(max - min + 1));
     }
-
+    
     @Override
     public void kick(Character c) {
-        int damage = new Random().nextInt(getPower()) + 1;
-        System.out.println(this.getClass().getSimpleName() + " deals " + damage + " damage to " + c.getClass().getSimpleName());
-        c.setHp(c.getHp() - damage);
+        c.setHp(c.getHp() - new Random().nextInt(getPower() + 1));
     }
 }
